ce31bff9d9a31628f3ff597ef0b51375a0098dc4
Gustavo Melo <gustavo.dsantosmelo@gmail.com>
2021-06-01 23:43:04 -0300

    Iniciando tela de credenciais

--- dashboard/src/App.vue
+++ dashboard/src/App.vue
@@ -7,8 +7,29 @@
 
 <script>
 import ModalFactory from '../src/components/ModalFactory'
+import { useRouter, useRoute } from 'vue-router'
+import { watch } from 'vue'
+import services from './services'
+import { setCurrentUser } from './store/user'
 
 export default {
-  components: { ModalFactory }
+  components: { ModalFactory },
+  setup () {
+    // const store = useStore()
+    const router = useRouter()
+    const route = useRoute()
+
+    watch(() => route.path, async () => {
+      if (route.meta.hasAuth) {
+        const token = window.localStorage.getItem('token')
+        if (!token) {
+          router.push({ name: 'Home' })
+          return
+        }
+        const { data } = await services.getMe()
+        setCurrentUser(data)
+      }
+    })
+  }
 }
 </script>
