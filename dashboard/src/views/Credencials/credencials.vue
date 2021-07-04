56fd25da108b256590b146a9880b254f0b6e14da
Gustavo Melo <gustavo.dsantosmelo@gmail.com>
2021-07-03 21:34:28 -0300

    Correção de erros da tela de credenciais

--- dashboard/src/views/Credencials/credencials.vue
+++ dashboard/src/views/Credencials/credencials.vue
@@ -1,10 +1,131 @@
 <template>
-  <h1>Aqui é o credencials</h1>
+  <div class="flex justify-center w-full h-28 bg-brand-main">
+    <HeaderLogged/>
+  </div>
+
+  <div class="flex flex-col h-64 justify-center items-center bg-brand-gray">
+    <h1 class="text-4xl font-black text-center text-gray-800">
+      Credenciais
+    </h1>
+    <p class="text-lg font-regular text-center text-gray-800">
+      Guia de instalação e geração de suas credenciais
+    </p>
+  </div>
+
+  <div class="flex w-full h-full justify-center">
+    <div class="flex flex-col max-w-6xl w-4/5">
+      <h1 class="text-3xl font-black text-brand-darkgray">
+        Instalação e configuração
+      </h1>
+      <p class="text-lg mt-10 font-regular text-gray-800">
+        Este aqui é a sua chave de api:
+      </p>
+      <content-loader
+      v-if="store.Global.isLoading || state.isLoading"
+      class="rounded"
+      width="600px"
+      heigth="50px"
+      />
+      <div
+      v-else
+      class="flex py-3 pl-5 pr-20 mt-2 rounded justify-between items-center bg-brand-gray w-full lg:w-1/2">
+        <span v-if="state.hasError">Erro ao carregar a apikey</span>
+        <span v-else>{{ store.User.currentUser.apiKey }}</span>
+        <div class="flex ml-20 mr-5" v-if="!state.hasError">
+          <icon
+          @click="handleCopy"
+          name="copy"
+          :color="brandColors.graydark"
+          size="24"
+          class="cursor-pointer"
+          />
+          <icon
+          @click="handleGenerateApikey"
+          name="loading"
+          :color="brandColors.graydark"
+          size="24"
+          class="cursor-pointer ml-3"
+          />
+        </div>
+      </div>
+      <p class="text-lg font-regular text-gray-800">
+        Coloque o script abaixo no seu site para começar a receber feedbacks:
+      </p>
+      <content-loader
+      v-if="store.Global.isLoading || state.isLoading"
+      class="rounded"
+      width="600px"
+      heigth="50px"
+      />
+      <div
+      v-else
+      class="py-3 pl-5 pr-20 mt-2 rounded bg-brand-gray overflow-x-scroll w-full lg:w-1/2">
+        <span v-if="state.hasError">Erro ao carregar o script</span>
+        <pre v-else>
+          &lt;script src="https://gustavo-melo-dev-feedbacker-widget.netlify.app?apikey={{store.User.currentUser.apiKey}}"&gt;&lt;/script&gt;
+        </pre>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import HeaderLogged from '@/components/HeaderLogged'
+import ContentLoader from '@/components/ContentLoader'
+import useStore from '@/hooks/useStore'
+import palette from '../../../palette'
+import Icon from '../../components/Icon'
+import { reactive } from '@vue/reactivity'
+import services from '../../services'
+import { setApiKey } from '../../store/user'
+import { watch } from '@vue/runtime-core'
+import { useToast } from 'vue-toastification'
 export default {
-
+  components: { HeaderLogged, Icon, ContentLoader },
+  setup () {
+    const store = useStore()
+    const toast = useToast()
+    const state = reactive({
+      hasError: false,
+      isLoading: false
+    })
+    watch(() => store.User.currentUser, () => {
+      if (!store.Global.isLoading && !store.User.currentUser.apiKey) {
+        handleError(true)
+      }
+    })
+    function handleError (error) {
+      state.isLoading = false
+      state.hasError = !!error
+    }
+    async function handleGenerateApikey () {
+      try {
+        state.isLoading = true
+        const { data } = await services.users.generateApikey()
+        setApiKey(data.apiKey)
+        state.isLoading = false
+      } catch (error) {
+        handleError(error)
+      }
+    }
+    async function handleCopy () {
+      toast.clear()
+      try {
+        await navigator.clipboard.writeText(store.User.currentUser.apiKey)
+        toast.success('Copiado!')
+      } catch (error) {
+        handleError(error)
+      }
+    }
+    return {
+      store,
+      brandColors: palette.brand,
+      state,
+      handleGenerateApikey,
+      handleCopy,
+      toast
+    }
+  }
 }
 </script>
 
